58c8e73f038c3b309813358d83fb32b74c5f7a88
Tim van Osch <timvosch@pollexprojects.nl>
2018-11-12 16:14:59 +0100

    Reference parameter

--- lib/Middleware/src/MiddlewareFunction.h
+++ lib/Middleware/src/MiddlewareFunction.h
@@ -2,6 +2,6 @@
 #define _MIDDLEWARE_FUNCTION_H_
 
 template<typename T>
-using MiddlewareFunctionPtr = bool(*)(T);
+using MiddlewareFunctionPtr = bool(*)(T*);
 
 #endif /* end of include guard: _MIDDLEWARE_FUNCTION_H_ */
