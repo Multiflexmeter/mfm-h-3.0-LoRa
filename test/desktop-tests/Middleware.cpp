67255a3d0718de671b9636289d0f494ffe67431f
Tim van Osch <timvosch@pollexprojects.nl>
2018-11-12 16:32:06 +0100

    Add Middleware test

--- test/desktop-tests/Middleware.cpp
+++ test/desktop-tests/Middleware.cpp
@@ -3,14 +3,42 @@
 #include <unity.h>
 #include <MiddlewareSystem.h>
 
-void test_Middleware_should_ShiftRight(void) {
-    // DUMMY TEST
-    TEST_ASSERT_EQUAL_HEX8(0xff, 0xff);
+bool middlewareFunction1(int* context) {
+    *context *= 2;
+    return true;
+}
+bool middlewareFunction2(int* context) {
+    *context -= 5;
+    return false;
+}
+bool middlewareFunction3(int* context) {
+    *context = 0;
+    return true;
+}
+
+/**
+ * Tests whether the middleware is added, executed in order, changing the context
+ * and continuing based on the return boolean.
+ * TODO: more specific tests
+ */
+void test_Middleware_should_ChangeContext(void) {
+    // Arrange
+    MiddlewareSystem<int, 3> system;
+    int context = 10;
+
+    // Act
+    system.add(&middlewareFunction1);
+    system.add(&middlewareFunction2);
+    system.add(&middlewareFunction3);
+    system.execute(&context);
+
+    // Assert
+    TEST_ASSERT_EQUAL_INT(15, context);
 }
 
 int main( int argc, char **argv) {
     UNITY_BEGIN();
-    RUN_TEST(test_Middleware_should_ShiftRight);
+    RUN_TEST(test_Middleware_should_ChangeContext);
     UNITY_END();
 }
 
