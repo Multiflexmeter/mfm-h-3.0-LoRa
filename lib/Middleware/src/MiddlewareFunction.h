93e0055f54707609fb512a0ce6f87584d6d47668
Tim van Osch <timvosch@pollexprojects.nl>
2018-11-12 16:00:02 +0100

    Add middleware function pointer

---
+++ lib/Middleware/src/MiddlewareFunction.h
@@ -0,0 +1,7 @@
+#ifndef _MIDDLEWARE_FUNCTION_H_
+#define _MIDDLEWARE_FUNCTION_H_
+
+template<typename T>
+using MiddlewareFunctionPtr = bool(*)(T);
+
+#endif /* end of include guard: _MIDDLEWARE_FUNCTION_H_ */
