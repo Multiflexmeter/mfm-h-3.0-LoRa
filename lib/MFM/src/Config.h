165100f81ffa808035e93727eee6e4150eeded3c
Tim van Osch <timvosch@pollexprojects.nl>
2018-11-19 17:54:52 +0100

    Remove unnecessary config

--- lib/MFM/src/Config.h
+++ lib/MFM/src/Config.h
@@ -1,12 +1,6 @@
 #ifndef _mfm_config_h_
 #define _mfm_config_h_
 
-/*
- *
- * The addresses in EEPROM where keys are stored.
- */
-#define APPEUI_ADDR 0x0
-#define DEVEUI_ADDR 0x8
-#define APPKEY_ADDR 0x16
+
 
 #endif /* end of include guard: _mfm_config_h_ */
