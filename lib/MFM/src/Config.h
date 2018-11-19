207f9eea6e0d9a7e0104562fcfc9bfda499a9f7d
Tim van Osch <timvosch@pollexprojects.nl>
2018-11-19 19:32:40 +0100

    Add sensors configuration

--- lib/MFM/src/Config.h
+++ lib/MFM/src/Config.h
@@ -1,6 +1,11 @@
 #ifndef _mfm_config_h_
 #define _mfm_config_h_
 
-
+/*
+ SENSORS
+ */
+#ifndef SENSORS_PIN_AMOUNT
+#define SENSORS_PIN_AMOUNT 4
+#endif
 
 #endif /* end of include guard: _mfm_config_h_ */
