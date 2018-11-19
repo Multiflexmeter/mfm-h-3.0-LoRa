003ec1999d9f85bc65ac65884817bb8e79ea8036
Tim van Osch <timvosch@pollexprojects.nl>
2018-11-19 17:43:11 +0100

    Improve documentation

--- lib/MFM/src/Settings.h
+++ lib/MFM/src/Settings.h
@@ -14,10 +14,14 @@ public:
      * @return a pointer to starting point of the reserves space
      */
     static int ReserveSpace(int size);
+    /**
+     * Resets the eepromTail to zero, essentially restarting reservation and
+     * losing all previous reservations.
+     */
     static void Reset();
 private:
+    /** The end address of used space */
     static int eepromTail;
-    // E2END defines last address
 };
 
 #endif /* end of include guard: _SETTINGS_H_ */
