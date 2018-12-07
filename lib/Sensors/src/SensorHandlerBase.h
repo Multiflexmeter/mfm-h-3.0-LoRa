2ac917cda6cf6c2d55b5017c47c52aa0a0844e42
Tim van Osch <timvosch@pollexprojects.nl>
2018-12-07 14:24:17 +0100

    Remove unnecessary reference parameter

--- lib/Sensors/src/SensorHandlerBase.h
+++ lib/Sensors/src/SensorHandlerBase.h
@@ -8,7 +8,7 @@ class SensorHandlerBase {
 public:
     virtual ~SensorHandlerBase() {}
     virtual unsigned short GetSignature() = 0;
-    virtual void ReadSensor(byte (&pins)[SENSOR_MAX_PINS], byte (&buffer)[SENSOR_READ_BUFFER_SIZE]) = 0;
+    virtual void ReadSensor(byte pins[SENSOR_MAX_PINS], byte (&buffer)[SENSOR_READ_BUFFER_SIZE]) = 0;
 };
 
 #endif /* end of include guard: _SENSOR_HANDLER_BASE_H_ */
