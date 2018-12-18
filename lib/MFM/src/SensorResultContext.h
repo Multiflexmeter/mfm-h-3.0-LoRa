159324a16f0af5dd8e84d4d97d98ea349fa727f9
Tim van Osch <timvosch@pollexprojects.nl>
2018-12-18 22:12:33 +0100

    Add object holding sensor results for middleware

---
+++ lib/MFM/src/SensorResultContext.h
@@ -0,0 +1,24 @@
+#ifndef _MFM_VECTOR_CONTEXT_H_
+#define _MFM_VECTOR_CONTEXT_H_
+
+#include <stdint.h>
+
+#include "Sensors.h"
+
+class SensorResult {
+public:
+    SensorResult();
+    uint16_t sensorType = 0xFFFF;
+    uint8_t sensorId = 0xFF;
+    uint8_t result[SENSOR_READ_BUFFER_SIZE] = {0};
+};
+
+// T is the amount of active sensors
+template<uint8_t T>
+class SensorResultContext {
+public:
+    uint8_t index = 0;
+    SensorResult results[T];
+};
+
+#endif
