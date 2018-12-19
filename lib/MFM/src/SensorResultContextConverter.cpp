ded4850ac50b129ca80331a4d7d836609f5496d4
Tim van Osch <timvosch@pollexprojects.nl>
2018-12-19 14:05:47 +0100

    Move placeholder(implementation) to MFM lib

--- lib/MFM/src/SensorResultContextConverter.cpp
+++ lib/MFM/src/SensorResultContextConverter.cpp
@@ -5,7 +5,8 @@
 // Easier reading
 using SensoResultContext_ = SensorResultContext<SENSOR_MAX_ENTRIES>;
 
-bool DataConverter<SensoResultContext_>::convert(SensoResultContext_ &data)
+template <>
+bool DataConverter::convert < SensoResultContext_>(SensoResultContext_ &data)
 {
     // Unused thus far, initial idea was huffman compression
 }
