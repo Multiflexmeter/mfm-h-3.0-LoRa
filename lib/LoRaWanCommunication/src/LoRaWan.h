ff7e536a0066621cf0dfab8b28ade83d886d1924
Tim van Osch <timvosch@pollexprojects.nl>
2018-12-20 20:41:18 +0100

    Add basic LoRawan lib

---
+++ lib/LoRaWanCommunication/src/LoRaWan.h
@@ -0,0 +1,16 @@
+#ifndef _MFM_LORAWAN_H_
+#define _MFM_LORAWAN_H_
+
+#include <lmic.h>
+#include <hal/hal.h>
+
+// TODO: Configuration for other frequencies
+
+class LoRaWan {
+private:
+
+public:
+    void Setup();
+};
+
+#endif
