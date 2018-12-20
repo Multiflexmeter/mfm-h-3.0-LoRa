81f3da864c6c2663a56e0a57edd7678602ce65bb
Tim van Osch <timvosch@pollexprojects.nl>
2018-12-20 17:19:49 +0100

    Add tests

--- test/test_native_dataconverter/DataConverter.cpp
+++ test/test_native_dataconverter/DataConverter.cpp
@@ -2,6 +2,7 @@
 
 #include <unity.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #include <DataConverter.h>
 
@@ -11,8 +12,11 @@ public:
     bool convert(int& data) {
         dataBytesLength = 4;
         dataBytes = new uint8_t[dataBytesLength];
-        // Copy int to array
-        memcpy(dataBytes, &data, 4);
+
+        dataBytes[0] = 0x11;
+        dataBytes[1] = 0x22;
+        dataBytes[2] = 0x33;
+        dataBytes[3] = 0x44;
     }
 };
 
@@ -29,11 +33,28 @@ void test_DataConverter_should_ReturnCorrectSize(void) {
     TEST_ASSERT_EQUAL_INT(4, arrayLength);
 }
 
+void test_DataConverter_should_ReturnCorrectDataBytes(void)
+{
+    // Arrange
+    IntDataConverter converter;
+    int ctx = 15;
+    converter.convert(ctx);
+    // Act
+    uint8_t resultBytes[converter.getDataSize()] = {0};
+
+    // Assert
+    TEST_ASSERT_EQUAL_HEX(0x11, converter.dataBytes[0]);
+    TEST_ASSERT_EQUAL_HEX(0x22, converter.dataBytes[1]);
+    TEST_ASSERT_EQUAL_HEX(0x33, converter.dataBytes[2]);
+    TEST_ASSERT_EQUAL_HEX(0x44, converter.dataBytes[3]);
+}
+
 // Entry point
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
     RUN_TEST(test_DataConverter_should_ReturnCorrectSize);
+    RUN_TEST(test_DataConverter_should_ReturnCorrectDataBytes);
     UNITY_END();
 }
 
